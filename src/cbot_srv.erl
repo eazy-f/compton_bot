53c7f8a0fc5397d3ce4b24adb88474e45bedd718
Nikita Lebedev <eazy-f@live.com>
2013-07-04 00:39:20 +0400

    Add some game state handling

--- src/cbot_srv.erl
+++ src/cbot_srv.erl
@@ -18,14 +18,24 @@ init({ Hostname, Port }) ->
 handle_info( {tcp, Socket, Data}, #state{game_server = Socket} = State)
   when State#state.state == login ->
     io:format("recv:~w~n", [Data]),
-    Name = "nigga",
+    Name = <<"nigga">>,
     ok = gen_tcp:send(Socket, hello_msg(Name)),
     { noreply, State#state{ name = Name, state = game } };
+
 handle_info( {tcp, Socket, Data}, #state{game_server = Socket} = State)
   when State#state.state == game ->
     io:format("recv:~w~n", [Data]),
-    ok = gen_tcp:send(Socket, make_turn()),
-    { noreply, State }.
+    case make_turn(Data, State#state.name) of
+        { reply, Reply } ->
+            ok = gen_tcp:send(Socket, Reply);
+        _ ->
+            ok
+    end,
+    { noreply, State };
+
+handle_info( {tcp_closed, Socket}, #state{game_server = Socket} = State) ->
+    io:format("connection closed"),
+    { stop, tcp_closed, State }.
 
 terminate(_, _) ->
     ok.
@@ -33,5 +43,12 @@ terminate(_, _) ->
 hello_msg(Name) ->
     mochijson2:encode({ struct, [ { "name", Name } ] }).
 
-make_turn() ->
-    mochijson2:encode({ struct, [ { "optionNumber", 0 } ] }).
+make_turn(GameState, Me) ->
+    { struct, State } = mochijson2:decode(GameState),
+    case proplists:get_value(<<"activePlayer">>, State) of
+        Me ->
+            Cmd = mochijson2:encode({ struct, [ { "optionNumber", 0 } ] }),
+            { reply, Cmd };
+        _ ->
+            noreply
+    end.
